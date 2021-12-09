d5e09c4da17acff9c47f05cc6f35b31f20f0b619
KavalryDev <raphael@kavalry.fr>
2021-12-09 13:42:06 +0100

    Last Update 09/12 13:41

---
+++ app/src/Views/Frontend/post.php
@@ -0,0 +1,12 @@
+<h1>Ceci est la page d'1 post</h1>
+
+<?php
+/**
+ * @var $posts \App\Entity\Post[]
+ */
+
+var_dump($post);
+
+echo "Bonjour";
+
+?>
---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3db8838c521e33b12807d1e14faad74006411b84',
+        'name' => 'raphaelbasilwylohn/blog',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'raphaelbasilwylohn/blog' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3db8838c521e33b12807d1e14faad74006411b84',
+            'dev_requirement' => false,
+        ),
+    ),
+);
